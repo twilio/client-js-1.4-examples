82becb6cee584ce82b37d611a9ebd15f26718a9b
Ryan Rowland <rrowland@twilio.com>
2017-03-30 16:23:27 -0700

    Volume Indicator

--- public/volume-indicators/js/main.js
+++ public/volume-indicators/js/main.js
@@ -1,91 +1,87 @@
 const callButton = document.getElementById('call-button');
-const hangupButton = document.getElementById('hangup-button');
-const incallButtons = document.getElementById('incall-buttons');
-const inputVolumeBar = document.getElementById('mic-volume');
-const precallButtons = document.getElementById('precall-buttons');
-const outputVolumeBar = document.getElementById('speaker-volume');
 const testMicButton = document.getElementById('test-mic-button');
 
-let activeConnection;
-let hasSetInput = false;
+// Create VolumeIndicators for input/output streams and append them to the page.
+const micIndicator = new VolumeIndicator();
+document.getElementById('mic-wrapper').appendChild(micIndicator.element);
 
-// Making a call is disabled until the Client is set up with a valid token.
+const speakerIndicator = new VolumeIndicator();
+document.getElementById('speaker-wrapper').appendChild(speakerIndicator.element);
+
+// Making a call and testing mic are disabled until the Client is set up with a valid token.
 getToken('alice').then(token => {
   Twilio.Device.ready(() => {
     callButton.disabled = false;
+    testMicButton.disabled = false;
   });
+
   Twilio.Device.setup(token);
 });
 
-// Render the input/output volume bars based on the current volume
-function renderVolume(volumeEl, volume) {
-  let color;
-  if (volume > 0.75) {
-    color = 'red';
-  } else if (volume > 0.50) {
-    color = 'yellow';
-  } else {
-    color = 'green';
-  }
-
-  volumeEl.style.width = `${Math.round(volume * 100)}%`;
-  volumeEl.style.backgroundColor = color;
-}
-
-const renderInputVolume = renderVolume.bind(null, inputVolumeBar);
-const renderOutputVolume = renderVolume.bind(null, outputVolumeBar);
-
-function renderBothVolumes(inputVolume, outputVolume) {
-  // Only render this input's volume if we're not already rendering the input stream
-  //   set by Device.audio.setInputDevice.
-  if (!hasSetInput) {
-    renderInputVolume(inputVolume);
-  }
-
-  renderOutputVolume(outputVolume);
-}
-
-// Make a call to the application specified in the token
-Twilio.Device.connect(conn => {
-  activeConnection = conn;
-  incallButtons.style.display = 'block';
-  precallButtons.style.display = 'none';
-  conn.on('volume', renderBothVolumes);
-  conn.disconnect(() => {
-    incallButtons.style.display = 'none';
-    precallButtons.style.display = 'block';
-    conn.removeListener('volume', renderBothVolumes);
-    renderBothVolumes(0, 0);
-  });
-});
+// Bind call button to make a call and set up volume indicators
+callButton.onclick = makeCall;
 
 function makeCall() {
+  stopMicTest();
   Twilio.Device.connect();
 }
 
-function hangUp() {
-  activeConnection && activeConnection.disconnect();
+Twilio.Device.connect(conn => {
+  showIncallUI();
+
+  conn.on('volume', setVolumes);
+
+  conn.disconnect(() => {
+    showPrecallUI();
+    conn.removeListener('volume', setVolumes);
+    setVolumes(0, 0);
+  });
+
+  document.getElementById('hangup-button').onclick = function() {
+    conn.disconnect();
+  }
+});
+
+function setVolumes(inputVolume, outputVolume) {
+  micIndicator.setVolume(inputVolume);
+  speakerIndicator.setVolume(outputVolume);
 }
 
-// Testing mic before making a call using Device.audio.
+// Bind Test Mic button to get user media and set up the volume indicator.
+testMicButton.onclick = startMicTest;
+
+function setInputVolume(volume) {
+  micIndicator.setVolume(volume);
+}
+
 function startMicTest() {
   Twilio.Device.audio.setInputDevice('default').then(() => {
-    Twilio.Device.audio.on('inputVolume', renderInputVolume);
+    Twilio.Device.audio.on('inputVolume', setInputVolume);
     testMicButton.innerText = 'Stop Testing';
-    testMicButton.onclick = stopMicTest;
-    hasSetInput = true;
+    testMicButton.onclick = function() {
+      Twilio.Device.audio.unsetInputDevice();
+      stopMicTest();
+    };
   });
 }
 
 function stopMicTest() {
-  Twilio.Device.audio.unsetInputDevice();
-  Twilio.Device.audio.removeListener('inputVolume', renderInputVolume);
+  Twilio.Device.audio.removeListener('inputVolume', setInputVolume);
   testMicButton.innerText = 'Test Mic';
   testMicButton.onclick = startMicTest;
-  renderInputVolume(0);
-  hasSetInput = false;
+  setInputVolume(0);
 }
 
-testMicButton.onclick = startMicTest;
-callButton.onclick = makeCall;
-hangupButton.onclick = hangUp;
+// UI stuff
+const incallButtons = document.getElementById('incall-buttons');
+const precallButtons = document.getElementById('precall-buttons');
+
+function showIncallUI() {
+  incallButtons.style.display = 'block';
+  precallButtons.style.display = 'none';
+}
+
+function showPrecallUI() {
+  incallButtons.style.display = 'none';
+  precallButtons.style.display = 'block';
+}
---
+++ public/volume-indicators/js/volumeIndicator.js
@@ -0,0 +1,94 @@
+/**
+ * Create a volume indicator element to attach to the DOM.
+ * @param {VolumeIndicatorOptions} [options]
+ * @property {HTMLDivElement} element - The HTML element to add to the page.
+ *//**
+ * @typedef {Object} VolumeIndicatorOptions
+ * @property {VolumeIndicator#determineColor} [determineColor] - An override for the default
+ *   determineColor method.
+ * @property {string} [height='100%'] - The CSS height to apply to the wrapper object, limiting the size
+ *   of the indicator. Defaults to 100% to fill its parent.
+ * @property {boolean} [vertical=false] - Whether the bar should be vertical (rather than horizontal).
+ * @property {string} [width='100%'] - The CSS width to apply to the wrapper object, limiting the size
+ *   of the indicator. Defaults to 100% to fill its parent.
+ */
+function VolumeIndicator(options) {
+  if (!(this instanceof VolumeIndicator)) {
+    return new VolumeIndicator(options);
+  }
+
+  options = Object.assign({
+    // Allow a custom document for testing headlessly.
+    _document: typeof document !== 'undefined' && document,
+    determineColor : defaultDetermineColor,
+    height: '100%',
+    vertical: false,
+    width: '100%'
+  }, options);
+
+  // Create the wrapper div that sets the boundaries for the volume bar. The volume bar
+  // will expand and contract to fill the wrapper.
+  const wrapperEl = options._document.createElement('div');
+  wrapperEl.style.width = options.width;
+  wrapperEl.style.height = options.height;
+  wrapperEl.style.display = 'block';
+  wrapperEl.style.position = 'relative';
+
+  // Create the volume bar itself.
+  const barEl = options._document.createElement('div');
+  barEl.style.display = 'block';
+  barEl.style.position = 'absolute';
+  wrapperEl.appendChild(barEl);
+
+  // Set the static side (width or height) of the volume bar to 100%, based on whether we are
+  // scaling vertically or horizontally.
+  const staticDimension = options.vertical ? 'width' : 'height';
+  barEl.style[staticDimension] = '100%';
+
+  // Anchor the volume bar to grow from the appropriate side.
+  const anchorSide = options.vertical ? 'bottom' : 'left';
+  barEl.style[anchorSide] = 0;
+
+  // Set all properties as read-only.
+  Object.defineProperties(this, {
+    _barElement: {
+      value: barEl
+    },
+    _determineColor: {
+      value: options.determineColor
+    },
+    _scaleDimension: {
+      value: options.vertical ? 'height' : 'width'
+    },
+    element: {
+      enumerable: true,
+      value: wrapperEl
+    }
+  });
+}
+
+/**
+ * Update the indicator with a new volume.
+ * @param {number} - A double (0.00 to 1.00 inclusive) representing the current volume.
+ * @return {void}
+ */
+VolumeIndicator.prototype.setVolume = function setVolume(volume) {
+  // Scale the bar to a percentage of the total available space vertically or horizontally.
+  this._barElement.style[this._scaleDimension] = `${Math.round(volume * 100)}%`;
+
+  // Set the color of the bar based on the determineColor method defined at construction.
+  this._barElement.style.backgroundColor = this._determineColor(volume);
+};
+
+/**
+ * @typedef {function} VolumeIndicator#determineColor
+ * @param {number} - A double (0.00 to 1.00 inclusive) representing the current volume.
+ * @returns {string} A color string to apply to the bar.
+ */
+function defaultDetermineColor(volume) {
+  if (volume > 0.75) { return 'red'; }
+  if (volume > 0.50) { return 'yellow'; }
+  return 'green';
+}
+
+window.VolumeIndicator = VolumeIndicator;
